5313cead0a90532bf9d33623809ca709ed113d6a
waruna <waruna@duosoftware.com>
2016-03-22 12:49:32 +0530

    with db con pool

--- DbHandler.go
+++ DbHandler.go
@@ -3,10 +3,12 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+
 	"fmt"
 	"github.com/go-contrib/uuid"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"golang.org/x/net/context"
 	"log"
 	"net/http"
 )
@@ -42,30 +44,220 @@ var TrackList = make(map[uuid.UUID][]string)
 var WorkersCount = make(map[uuid.UUID]int)
 var errorList = make(map[uuid.UUID][]string)
 
-func GetTrackingInfo(res http.ResponseWriter, req *http.Request) {
+func AppendToTrackList(uid uuid.UUID) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in AppendToTrackList", r)
+		}
+	}()
+	TrackList[uid] = append(TrackList[uid], "done")
+	fmt.Println("AppendToTrackList : ", TrackList[uid])
+}
+
+//ctx context.Context, w http.ResponseWriter, r *http.Request
+func UploadContactsToCampaignAndAttachSchedule(ctx context.Context, res http.ResponseWriter, req *http.Request) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in UploadContactsToCampaignAndAttachSchedule", r)
+		}
+	}()
+	res.Header().Set("Content-Type", "application/json")
+	if req.Method != "POST" {
+		fmt.Println("405")
+		http.Error(res, http.StatusText(405), 405)
+		return
+	}
+	db, ok := ctx.Value("db").(*sql.DB)
+	if !ok {
+		fmt.Println("Recovered in UploadContactsToCampaignAndAttachSchedule")
+	}
+
+	var gophers = 10
+
+	fmt.Println("------------------ UploadContactsToCampaignAndAttachSchedule ------------------")
+	uploadData := UploadData{}
+	decoder := json.NewDecoder(req.Body)
+	error := decoder.Decode(&uploadData)
+	if error != nil {
+		log.Println(error.Error())
+		http.Error(res, error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("Upload Data : ", uploadData)
+	entries := len(uploadData.Contacts)
+	page := entries / gophers
+
+	u1 := uuid.NewV4()
+	fmt.Printf("UUIDv4: %s\n", u1)
+	TrackList[u1] = []string{}
+	WorkersCount[u1] = page
+
+	if page == 0 {
+		WorkersCount[u1] = 1
+	}
+
+	fmt.Println("page Count : ", page)
+	if 1 >= page {
+		gophers = 1
+	}
+	j := 0
+	// run the insert function using 10 go routines
+	for i := 0; i < gophers; i++ {
+		contacts := uploadData.Contacts[j : j+page]
+
+		if (i == gophers-1) && (j < entries) {
+			contacts = uploadData.Contacts[j:entries]
+		}
+
+		// spin up a gopher
+		go SaveToDb(i, contacts, u1, uploadData, gophers, db)
+		j = j + page
+	}
+
+	uploadData.TrackerId = u1
+	outgoingJSON, err := json.Marshal(uploadData)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	res.WriteHeader(http.StatusCreated)
+	fmt.Fprint(res, string(outgoingJSON))
+	fmt.Println("------------------ UploadContactsToCampaignAndAttachSchedule. end ------------------", u1)
+}
+
+func SaveToDb(gopher_id int, contacts []string, uid uuid.UUID, data UploadData, gophers int, db *sql.DB) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in SaveToDb", r)
+		}
+	}()
+
+	fmt.Print("Gopher Id : ", gopher_id)
+	fmt.Println("  contacts : ", contacts)
+
+	db.SetMaxOpenConns(gophers)
+
+	// create string to pass
+	var sStmt string = "WITH i AS (INSERT INTO \"DB_CAMP_ContactInfos\"(\"ContactId\", \"CategoryID\", \"TenantId\", \"CompanyId\",\"createdAt\" ,\"updatedAt\" ) VALUES ($1, $2, $3, $4,now(),now()) RETURNING \"CamContactId\") INSERT INTO \"DB_CAMP_ContactSchedules\"(\"CampaignId\", \"CamScheduleId\", \"ExtraData\",\"createdAt\" ,\"updatedAt\", \"CamContactId\")    VALUES ($5, $6, $7,now(),now(), (SELECT \"CamContactId\" FROM i))" // "WITH i AS (INSERT INTO tb1a (t) VALUES ($1) RETURNING id) INSERT INTO tb1b (t)SELECT id FROM i "
+	//"insert into test (gopher_id, created) values ($1, $2)" //"insert into test (gopher_id, created) values ($1, $2)" //
+	stmt, err := db.Prepare(sStmt)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for i := 0; i < len(contacts); i++ {
+		//ContactId\", \"CategoryID\", \"TenantId\", \"CompanyId CampaignId\", \"CamScheduleId\", \"ExtraData
+		res, err := stmt.Exec(contacts[i], data.CategoryId, data.TenantId, data.CompanyId, data.CampaignId, data.CamScheduleId, data.ExtraData)
+		if err != nil || res == nil {
+			log.Print(err)
+			errorList[uid] = append(errorList[uid], contacts[i])
+		}
+		defer stmt.Close()
+	}
+
+	AppendToTrackList(uid)
+}
+
+//---------------------Assing Exssiting Numbers To Campaign--------------------------\\
+
+func AssingExssitingNumbersToCampaign(ctx context.Context, res http.ResponseWriter, req *http.Request) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in AssingExssitingNumbersToCampaign", r)
+		}
+	}()
+
+	res.Header().Set("Content-Type", "application/json")
+	if req.Method != "POST" {
+		fmt.Println("405")
+		http.Error(res, http.StatusText(405), 405)
+		return
+	}
+	db, ok := ctx.Value("db").(*sql.DB)
+	if !ok {
+		fmt.Println("Recovered in UploadContactsToCampaignAndAttachSchedule")
+	}
+	vars := mux.Vars(req)
+
+	campaignId := vars["CampaignId"]
+
+	existingData := new(ExistingData)
+	decoder := json.NewDecoder(req.Body)
+	error := decoder.Decode(&existingData)
+	if error != nil {
+		log.Println(error.Error())
+		http.Error(res, error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	db.SetMaxOpenConns(1)
+
+	// create string to pass
+	var sStmt string = "INSERT INTO \"DB_CAMP_ContactSchedules\"(\"CampaignId\", \"CamScheduleId\", \"CamContactId\", \"ExtraData\",\"createdAt\", \"updatedAt\") SELECT $1, $2,\"CamContactId\" , $3,now(),now() FROM \"DB_CAMP_ContactInfos\" WHERE  \"CategoryID\"=$4"
+
+	//"INSERT INTO \"DB_CAMP_ContactSchedules\"(\"CampaignId\", \"CamScheduleId\", \"CamContactId\", \"ExtraData\",\"createdAt\", \"updatedAt\") SELECT  " + CampaignId + ", " + existingData.CamScheduleId + ", 'CamContactId', " + existingData.extraData + ",now(),now() FROM \"DB_CAMP_ContactInfos\" WHERE  \"CategoryID\"=" + existingData.CategoryID
+
+	msg := "Process Complete."
+	stmt, err := db.Prepare(sStmt)
+	if err != nil {
+		log.Panic(err)
+		msg = "Error"
+	}
+
+	res.WriteHeader(http.StatusCreated)
+	reply, err := stmt.Exec(campaignId, existingData.CamScheduleId, existingData.ExtraData, existingData.CategoryID)
+	if err != nil || reply == nil {
+		msg = "Error"
+		fmt.Fprint(res, err.Error())
+		log.Panic(err)
+	}
+
+	stmt.Close()
+
+	fmt.Fprint(res, msg)
+
+}
+
+//---------------------End-Assing Exssiting Numbers To Campaign--------------------------\\
+
+func GetTrackingInfo(ctx context.Context, res http.ResponseWriter, req *http.Request) {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Recovered in GetTrackingInfo", r)
 		}
 	}()
 
+	res.Header().Set("Content-Type", "application/json")
+	if req.Method != "GET" {
+		fmt.Println("405")
+		http.Error(res, http.StatusText(405), 405)
+		return
+	}
+
 	fmt.Println("------------- Tracking Info -----------------")
 	fmt.Println(" workerCount : ", WorkersCount)
 	fmt.Println(" TrackList : ", TrackList)
 	fmt.Println(" errorList : ", errorList)
 	fmt.Println("------------------------------")
-	res.Header().Set("Content-Type", "application/json")
 	res.WriteHeader(http.StatusCreated)
 	fmt.Fprint(res, "{\"msg\": \"Done\"}")
 
 }
 
-func RemoveCompleteProcess(res http.ResponseWriter, req *http.Request) {
+func RemoveCompleteProcess(ctx context.Context, res http.ResponseWriter, req *http.Request) {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Recovered in RemoveCompleteProcess", r)
 		}
 	}()
+	res.Header().Set("Content-Type", "application/json")
+	if req.Method != "POST" {
+		fmt.Println("405")
+		http.Error(res, http.StatusText(405), 405)
+		return
+	}
 	list := []uuid.UUID{}
 	for index, element := range WorkersCount {
 		fmt.Println("index", index)
@@ -78,7 +270,7 @@ func RemoveCompleteProcess(res http.ResponseWriter, req *http.Request) {
 			list = append(list, index)
 		}
 	}
-	res.Header().Set("Content-Type", "application/json")
+
 	outgoingJSON, err := json.Marshal(list)
 	if err != nil {
 		log.Println(err.Error())
@@ -89,12 +281,18 @@ func RemoveCompleteProcess(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(res, string(outgoingJSON))
 }
 
-func TrackNumberUpload(res http.ResponseWriter, req *http.Request) {
+func TrackNumberUpload(ctx context.Context, res http.ResponseWriter, req *http.Request) {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Recovered in TrackNumberUpload", r)
 		}
 	}()
+	res.Header().Set("Content-Type", "application/json")
+	if req.Method != "GET" {
+		fmt.Println("405")
+		http.Error(res, http.StatusText(405), 405)
+		return
+	}
 	vars := mux.Vars(req)
 
 	trackerId, err := uuid.FromString(vars["TrackerId"])
@@ -134,173 +332,3 @@ func TrackNumberUpload(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(res, string(outgoingJSON))
 
 }
-
-func AppendToTrackList(uid uuid.UUID) {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered in AppendToTrackList", r)
-		}
-	}()
-	TrackList[uid] = append(TrackList[uid], "done")
-	fmt.Println("AppendToTrackList : ", TrackList[uid])
-}
-
-func UploadContactsToCampaignAndAttachSchedule(res http.ResponseWriter, req *http.Request) {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered in UploadContactsToCampaignAndAttachSchedule", r)
-		}
-	}()
-	var gophers = 10
-	res.Header().Set("Content-Type", "application/json")
-	fmt.Println("------------------ UploadContactsToCampaignAndAttachSchedule ------------------")
-	uploadData := UploadData{}
-	decoder := json.NewDecoder(req.Body)
-	error := decoder.Decode(&uploadData)
-	if error != nil {
-		log.Println(error.Error())
-		http.Error(res, error.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Println("Upload Data : ", uploadData)
-	entries := len(uploadData.Contacts)
-	page := entries / gophers
-
-	u1 := uuid.NewV4()
-	fmt.Printf("UUIDv4: %s\n", u1)
-	TrackList[u1] = []string{}
-	WorkersCount[u1] = page
-
-	if page == 0 {
-		WorkersCount[u1] = 1
-	}
-
-	fmt.Println("page Count : ", page)
-	if 1 >= page {
-		gophers = 1
-	}
-	j := 0
-	// run the insert function using 10 go routines
-	for i := 0; i < gophers; i++ {
-		contacts := uploadData.Contacts[j : j+page]
-
-		if (i == gophers-1) && (j < entries) {
-			contacts = uploadData.Contacts[j:entries]
-		}
-
-		// spin up a gopher
-		go SaveToDb(i, contacts, u1, uploadData, gophers)
-		j = j + page
-	}
-
-	uploadData.TrackerId = u1
-	outgoingJSON, err := json.Marshal(uploadData)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(res, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	res.WriteHeader(http.StatusCreated)
-	fmt.Fprint(res, string(outgoingJSON))
-	fmt.Println("------------------ UploadContactsToCampaignAndAttachSchedule. end ------------------", u1)
-}
-
-func SaveToDb(gopher_id int, contacts []string, uid uuid.UUID, data UploadData, gophers int) {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered in SaveToDb", r)
-		}
-	}()
-
-	fmt.Print("Gopher Id : ", gopher_id)
-	fmt.Println("  contacts : ", contacts)
-
-	// lazily open db (doesn't truly open until first request)
-	db, err := sql.Open("postgres", "host=localhost dbname=dvpdb sslmode=disable user=duouser password=DuoS123")
-	if err != nil {
-		log.Panic(err)
-	}
-	db.SetMaxOpenConns(gophers)
-
-	// create string to pass
-	var sStmt string = "WITH i AS (INSERT INTO \"DB_CAMP_ContactInfos\"(\"ContactId\", \"CategoryID\", \"TenantId\", \"CompanyId\",\"createdAt\" ,\"updatedAt\" ) VALUES ($1, $2, $3, $4,now(),now()) RETURNING \"CamContactId\") INSERT INTO \"DB_CAMP_ContactSchedules\"(\"CampaignId\", \"CamScheduleId\", \"ExtraData\",\"createdAt\" ,\"updatedAt\", \"CamContactId\")    VALUES ($5, $6, $7,now(),now(), (SELECT \"CamContactId\" FROM i))" // "WITH i AS (INSERT INTO tb1a (t) VALUES ($1) RETURNING id) INSERT INTO tb1b (t)SELECT id FROM i "
-	//"insert into test (gopher_id, created) values ($1, $2)" //"insert into test (gopher_id, created) values ($1, $2)" //
-	stmt, err := db.Prepare(sStmt)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	for i := 0; i < len(contacts); i++ {
-		//ContactId\", \"CategoryID\", \"TenantId\", \"CompanyId CampaignId\", \"CamScheduleId\", \"ExtraData
-		res, err := stmt.Exec(contacts[i], data.CategoryId, data.TenantId, data.CompanyId, data.CampaignId, data.CamScheduleId, data.ExtraData)
-		if err != nil || res == nil {
-			log.Print(err)
-			errorList[uid] = append(errorList[uid], contacts[i])
-		}
-		defer stmt.Close()
-	}
-
-	// close db
-	defer db.Close()
-	AppendToTrackList(uid)
-}
-
-//---------------------Assing Exssiting Numbers To Campaign--------------------------\\
-
-func AssingExssitingNumbersToCampaign(res http.ResponseWriter, req *http.Request) {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered in AssingExssitingNumbersToCampaign", r)
-		}
-	}()
-
-	vars := mux.Vars(req)
-
-	campaignId := vars["CampaignId"]
-
-	existingData := new(ExistingData)
-	decoder := json.NewDecoder(req.Body)
-	error := decoder.Decode(&existingData)
-	if error != nil {
-		log.Println(error.Error())
-		http.Error(res, error.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// lazily open db (doesn't truly open until first request)
-	db, err := sql.Open("postgres", "host=localhost dbname=dvpdb sslmode=disable user=duouser password=DuoS123")
-	if err != nil {
-		log.Panic(err)
-	}
-	db.SetMaxOpenConns(1)
-
-	// create string to pass
-	var sStmt string = "INSERT INTO \"DB_CAMP_ContactSchedules\"(\"CampaignId\", \"CamScheduleId\", \"CamContactId\", \"ExtraData\",\"createdAt\", \"updatedAt\") SELECT $1, $2,\"CamContactId\" , $3,now(),now() FROM \"DB_CAMP_ContactInfos\" WHERE  \"CategoryID\"=$4"
-
-	//"INSERT INTO \"DB_CAMP_ContactSchedules\"(\"CampaignId\", \"CamScheduleId\", \"CamContactId\", \"ExtraData\",\"createdAt\", \"updatedAt\") SELECT  " + CampaignId + ", " + existingData.CamScheduleId + ", 'CamContactId', " + existingData.extraData + ",now(),now() FROM \"DB_CAMP_ContactInfos\" WHERE  \"CategoryID\"=" + existingData.CategoryID
-
-	msg := "Process Complete."
-	stmt, err := db.Prepare(sStmt)
-	if err != nil {
-		log.Panic(err)
-		msg = "Error"
-	}
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusCreated)
-	reply, err := stmt.Exec(campaignId, existingData.CamScheduleId, existingData.ExtraData, existingData.CategoryID)
-	if err != nil || reply == nil {
-		msg = "Error"
-		fmt.Fprint(res, err.Error())
-		log.Panic(err)
-	}
-
-	stmt.Close()
-	// close db
-	db.Close()
-
-	fmt.Fprint(res, msg)
-
-}
-
-//---------------------End-Assing Exssiting Numbers To Campaign--------------------------\\
--- main.go
+++ main.go
@@ -3,17 +3,52 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"fmt"
+	"golang.org/x/net/context"
+	"log"
 	"net/http"
 )
 
+type ContextHandler interface {
+	ServeHTTPContext(context.Context, http.ResponseWriter, *http.Request)
+}
+
+type ContextHandlerFunc func(context.Context, http.ResponseWriter, *http.Request)
+
+func (h ContextHandlerFunc) ServeHTTPContext(ctx context.Context, rw http.ResponseWriter, req *http.Request) {
+	h(ctx, rw, req)
+}
+
+type ContextAdapter struct {
+	ctx     context.Context
+	handler ContextHandler
+}
+
+func (ca *ContextAdapter) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	ca.handler.ServeHTTPContext(ca.ctx, rw, req)
+}
+
 func main() {
 
-	router := mux.NewRouter()
-	router.HandleFunc("/DVP/API/6.0/CampaignManager/NumberUpload", UploadContactsToCampaignAndAttachSchedule).Methods("POST")
-	router.HandleFunc("/DVP/API/6.0/CampaignManager/NumberUpload/RemoveComplete", RemoveCompleteProcess).Methods("POST")
-	router.HandleFunc("/DVP/API/6.0/CampaignManager/NumberUpload/TrackerInfo", GetTrackingInfo).Methods("GET")
-	router.HandleFunc("/DVP/API/6.0/CampaignManager/NumberUpload/{TrackerId}", TrackNumberUpload).Methods("GET")
-	router.HandleFunc("/DVP/API/6.0/CampaignManager/NumberUpload/{CampaignId}/Numbers/Existing", AssingExssitingNumbersToCampaign).Methods("POST")
-	http.ListenAndServe(":8080", router)
+	db, err := NewDB()
+	if err != nil {
+		log.Panic(err)
+	}
+	ctx := context.WithValue(context.Background(), "db", db)
+	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload", &ContextAdapter{ctx, ContextHandlerFunc(UploadContactsToCampaignAndAttachSchedule)})
+	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/RemoveComplete", &ContextAdapter{ctx, ContextHandlerFunc(RemoveCompleteProcess)})
+	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/TrackerInfo", &ContextAdapter{ctx, ContextHandlerFunc(GetTrackingInfo)})
+	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/{TrackerId}", &ContextAdapter{ctx, ContextHandlerFunc(TrackNumberUpload)})
+	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/{CampaignId}/Numbers/Existing", &ContextAdapter{ctx, ContextHandlerFunc(AssingExssitingNumbersToCampaign)})
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func booksIndex(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("AppendToTrackList : ")
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+	UploadContactsToCampaignAndAttachSchedule(ctx, w, r)
 }
