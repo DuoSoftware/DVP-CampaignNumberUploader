8b5693feae0efaede3d5c6dc95f4bc6c6a366775
waruna <waruna@duosoftware.com>
2016-03-22 13:01:05 +0530

    with def package

--- main.go
+++ main.go
@@ -3,7 +3,7 @@
 package main
 
 import (
-	"fmt"
+	"DVP-CampaignNoUploader/models"
 	"golang.org/x/net/context"
 	"log"
 	"net/http"
@@ -30,25 +30,16 @@ func (ca *ContextAdapter) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 func main() {
 
-	db, err := NewDB()
+	db, err := models.NewDB()
 	if err != nil {
 		log.Panic(err)
 	}
 	ctx := context.WithValue(context.Background(), "db", db)
-	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload", &ContextAdapter{ctx, ContextHandlerFunc(UploadContactsToCampaignAndAttachSchedule)})
-	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/RemoveComplete", &ContextAdapter{ctx, ContextHandlerFunc(RemoveCompleteProcess)})
-	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/TrackerInfo", &ContextAdapter{ctx, ContextHandlerFunc(GetTrackingInfo)})
-	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/{TrackerId}", &ContextAdapter{ctx, ContextHandlerFunc(TrackNumberUpload)})
-	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/{CampaignId}/Numbers/Existing", &ContextAdapter{ctx, ContextHandlerFunc(AssingExssitingNumbersToCampaign)})
+	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload", &ContextAdapter{ctx, ContextHandlerFunc(models.UploadContactsToCampaignAndAttachSchedule)})
+	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/RemoveComplete", &ContextAdapter{ctx, ContextHandlerFunc(models.RemoveCompleteProcess)})
+	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/TrackerInfo", &ContextAdapter{ctx, ContextHandlerFunc(models.GetTrackingInfo)})
+	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/{TrackerId}", &ContextAdapter{ctx, ContextHandlerFunc(models.TrackNumberUpload)})
+	http.Handle("/DVP/API/6.0/CampaignManager/NumberUpload/{CampaignId}/Numbers/Existing", &ContextAdapter{ctx, ContextHandlerFunc(models.AssingExssitingNumbersToCampaign)})
 	http.ListenAndServe(":8080", nil)
 
-}
-
-func booksIndex(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	fmt.Println("AppendToTrackList : ")
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(405), 405)
-		return
-	}
-	UploadContactsToCampaignAndAttachSchedule(ctx, w, r)
 }
